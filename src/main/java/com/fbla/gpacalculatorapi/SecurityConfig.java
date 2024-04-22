22b219605cb0d35b160c265772b02ec423dca609
Rithvika Devisetti <70250513+rithvika1221@users.noreply.github.com>
2024-04-21 19:44:57 -0700

    Security Changes

---
+++ src/main/java/com/fbla/gpacalculatorapi/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.fbla.gpacalculatorapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+      @Bean
+      public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+          httpSecurity.csrf().disable()
+                  .authorizeHttpRequests()
+                  .anyRequest()
+                  .authenticated()
+                  .and()
+                  .httpBasic();
+          return httpSecurity.build();
+      }
+      @Bean
+      public InMemoryUserDetailsManager userDetailsService() {
+          UserDetails user = User.withDefaultPasswordEncoder()
+                  .username("rithvika")
+                  .password("seattle@123")
+                  .roles("ADMIN")
+                  .build();
+          return new InMemoryUserDetailsManager(user);
+      }
+}
